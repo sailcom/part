9c97e4b8cecdf1846b14109cf4f99c61d462f929
肥牛 <42264621+sailcom@users.noreply.github.com>
2024-11-02 20:25:04 +0800

    Update wcjz_sign.js

--- wcjz_sign.js
+++ wcjz_sign.js
@@ -1,7 +1,7 @@
 var wc = JSON.parse($response.body);
 console.log("原body：");
 console.log(wc);
-wc.data.response = "{\"userInfo\":{\"avatarUrl\":\"http:\/\/avatar.wacdn.com\/d4a508a6701f40e280467d9a4447143a\",\"nickName\":\"sailcom\"},\"uid\":1498214,\"vipInfo\":{\"adFreePermanentVip\":true,\"adFreeVipEnable\":0,\"vipType\":2,\"freeSendVipEnable\":0,\"matchVipTrial\":true,\"expireDaysDays\":null,\"freeSendAdFreeVipEnable\":0,\"continuousEnable\":0,\"continuousType\":0,\"expirationDate\":4084380366000,\"superExpireDate\":4084380366000,\"adFreeExpireDate\":4084380366000,\"normalExpireDate\":4084380366000,\"isPermanentVip\":true,\"vipMemberEnable\":0},\"ets\":1731152867216}";
+wc.data.response = "{\"userInfo\":{\"avatarUrl\":\"http:\/\/avatar.wacdn.com\/d4a508a6701f40e280467d9a4447143a\",\"nickName\":\"sail\"},\"uid\":1498214,\"vipInfo\":{\"adFreePermanentVip\":true,\"adFreeVipEnable\":0,\"vipType\":2,\"freeSendVipEnable\":0,\"matchVipTrial\":true,\"expireDaysDays\":null,\"freeSendAdFreeVipEnable\":0,\"continuousEnable\":0,\"continuousType\":0,\"expirationDate\":4084380366000,\"superExpireDate\":4084380366000,\"adFreeExpireDate\":4084380366000,\"normalExpireDate\":4084380366000,\"isPermanentVip\":true,\"vipMemberEnable\":0},\"ets\":1731152867216}";
 console.log("替换后：");
 console.log(wc);
 $done({body: JSON.stringify(wc)}); 
@@ -10,7 +10,6 @@ $done({body: JSON.stringify(wc)});
 guding = {
   "code": 0,
   "data": {
-    "sign": "yAREdgPUaq9Q27uQd8PUfWnAOav84JIi4nqjVOU1\/txpx8BUZ6J5BuvZlXi2DhPeB\/Vt\/w\/siCtL\/ZO0wWyij3hU\/t\/PM7QiOEoHCvBTscw3ylTS+iMBjK5ydAq6cWVde2M2CSL5wj7cVGyQVFU+c3CIQAExXcVDYdrtsAR0nGM=",
     "response": {
       "userInfo": {
         "avatarUrl": "https:\/\/raw\.githubusercontent\.com\/Guding88\/iCon\/main\/png\/Guding\.png",
