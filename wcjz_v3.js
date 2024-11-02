284838562dac730ef826b76df1a6c86f8d65ebeb
肥牛 <42264621+sailcom@users.noreply.github.com>
2024-11-02 20:24:15 +0800

    Update wcjz_v3.js

--- wcjz_v3.js
+++ wcjz_v3.js
@@ -6,7 +6,6 @@ wcc.data.vipInfo.isVipMember = true;
 wcc.data.vipInfo.expirationDate = 4084380366000;
 wcc.data.vipInfo.remainingDays = 666;
 wcc.data.vipInfo.consecutiveDays = 8888;
-//wc.data.response = "{\"userInfo\":{\"avatarUrl\":\"http:\/\/avatar.wacdn.com\/d4a508a6701f40e280467d9a4447143a\",\"nickName\":\"sailcom\"},\"uid\":1498214,\"vipInfo\":{\"adFreePermanentVip\":true,\"adFreeVipEnable\":0,\"vipType\":2,\"freeSendVipEnable\":0,\"matchVipTrial\":true,\"expireDaysDays\":null,\"freeSendAdFreeVipEnable\":0,\"continuousEnable\":0,\"continuousType\":0,\"expirationDate\":4084380366000,\"superExpireDate\":4084380366000,\"adFreeExpireDate\":4084380366000,\"normalExpireDate\":4084380366000,\"isPermanentVip\":true,\"vipMemberEnable\":0},\"ets\":1731152867216}";
 console.log("替换后_v3：");
 console.log(wcc);
 $done({body: JSON.stringify(wcc)}); 
@@ -15,7 +14,6 @@ $done({body: JSON.stringify(wcc)});
 guding = {
   "code": 0,
   "data": {
-    "sign": "yAREdgPUaq9Q27uQd8PUfWnAOav84JIi4nqjVOU1\/txpx8BUZ6J5BuvZlXi2DhPeB\/Vt\/w\/siCtL\/ZO0wWyij3hU\/t\/PM7QiOEoHCvBTscw3ylTS+iMBjK5ydAq6cWVde2M2CSL5wj7cVGyQVFU+c3CIQAExXcVDYdrtsAR0nGM=",
     "response": {
       "userInfo": {
         "avatarUrl": "https:\/\/raw\.githubusercontent\.com\/Guding88\/iCon\/main\/png\/Guding\.png",
