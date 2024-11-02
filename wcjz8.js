64b57eff4442a61e07cb7752b7703d7909a9663f
肥牛 <42264621+sailcom@users.noreply.github.com>
2024-11-02 19:43:04 +0800

    Update and rename wcjz6.js to wcjz8.js

--- wcjz8.js
+++ wcjz8.js
@@ -1,8 +1,7 @@
 var wc = JSON.parse($response.body);
 console.log("原body：");
 console.log(wc);
-wc.data.response = "{\"userInfo\":{\"avatarUrl\":\"http:\/\/avatar.wacdn.com\/d4a508a6701f40e280467d9a4447143a\",\"nickName\":\"sail666\"},\"uid\":1498214,\"vipInfo\":{\"adFreePermanentVip\":true,\"adFreeVipEnable\":0,\"vipType\":2,\"freeSendVipEnable\":0,\"matchVipTrial\":true,\"expireDaysDays\":null,\"freeSendAdFreeVipEnable\":0,\"continuousEnable\":0,\"continuousType\":0,\"expirationDate\":4084380366000,\"isPermanentVip\":true,\"vipMemberEnable\":0},\"ets\":0}";
-//wc.data.response = '{"userInfo":{"avatarUrl":"http://avatar.wacdn.com/d4a508a6701f40e280467d9a4447143a","nickName":"sail666"},"uid":1498214,"vipInfo":{"adFreePermanentVip":true,"adFreeVipEnable":0,"vipType":2,"freeSendVipEnable":0,"matchVipTrial":true,"expireDaysDays":null,"freeSendAdFreeVipEnable":0,"continuousEnable":0,"continuousType":0,"expirationDate":4084380366000,"isPermanentVip":true,"vipMemberEnable":0},"ets":0}';
+wc.data.response = "{\"userInfo\":{\"avatarUrl\":\"http:\/\/avatar.wacdn.com\/d4a508a6701f40e280467d9a4447143a\",\"nickName\":\"sailcom\"},\"uid\":1498214,\"vipInfo\":{\"adFreePermanentVip\":true,\"adFreeVipEnable\":0,\"vipType\":2,\"freeSendVipEnable\":0,\"matchVipTrial\":true,\"expireDaysDays\":null,\"freeSendAdFreeVipEnable\":0,\"continuousEnable\":0,\"continuousType\":0,\"expirationDate\":4084380366000,\"superExpireDate\":4084380366000,\"adFreeExpireDate\":4084380366000,\"normalExpireDate\":4084380366000,\"isPermanentVip\":true,\"vipMemberEnable\":0},\"ets\":0}";
 console.log("替换后：");
 console.log(wc);
 $done({body: JSON.stringify(wc)}); 
