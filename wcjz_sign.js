ec143af89beba289f268f1ca035313d05f678773
肥牛 <42264621+sailcom@users.noreply.github.com>
2024-11-02 21:00:39 +0800

    Update wcjz_sign.js

--- wcjz_sign.js
+++ wcjz_sign.js
@@ -1,42 +1,7 @@
 var wc = JSON.parse($response.body);
 console.log("原body：");
 console.log(wc);
-wc.data.response = "{\"userInfo\":{\"avatarUrl\":\"http:\/\/avatar.wacdn.com\/d4a508a6701f40e280467d9a4447143a\",\"nickName\":\"sail\"},\"uid\":1498214,\"vipInfo\":{\"adFreePermanentVip\":true,\"adFreeVipEnable\":0,\"vipType\":2,\"freeSendVipEnable\":0,\"matchVipTrial\":true,\"expireDaysDays\":null,\"freeSendAdFreeVipEnable\":0,\"continuousEnable\":0,\"continuousType\":0,\"expirationDate\":4084380366000,\"superExpireDate\":4084380366000,\"adFreeExpireDate\":4084380366000,\"normalExpireDate\":4084380366000,\"isPermanentVip\":true,\"vipMemberEnable\":0},\"ets\":1731152867216}";
+wc.data.response = "{\"userInfo\":{\"avatarUrl\":\"http:\/\/avatar.wacdn.com\/d4a508a6701f40e280467d9a4447143a\",\"nickName\":\"sail\"},\"uid\":1498214,\"vipInfo\":{\"adFreePermanentVip\":true,\"adFreeVipEnable\":1,\"vipType\":2,\"freeSendVipEnable\":0,\"matchVipTrial\":true,\"expireDaysDays\":99999,\"freeSendAdFreeVipEnable\":0,\"continuousEnable\":1,\"continuousType\":1,\"expirationDate\":4092599349000,\"superExpireDate\":4092599349000,\"adFreeExpireDate\":4092599349000,\"normalExpireDate\":4092599349000,\"isPermanentVip\":true,\"vipMemberEnable\":1},\"ets\":1731152867216}";
 console.log("替换后：");
 console.log(wc);
 $done({body: JSON.stringify(wc)}); 
-
-/*
-guding = {
-  "code": 0,
-  "data": {
-    "response": {
-      "userInfo": {
-        "avatarUrl": "https:\/\/raw\.githubusercontent\.com\/Guding88\/iCon\/main\/png\/Guding\.png",
-        "nickName": "sail666"
-      },
-      "uid":1498214,
-      "vipInfo": {
-        "adFreeVipEnable": 0,
-        "matchVipTrial": true,
-        "adFreePermanentVip": true,
-        "vipMemberEnable": 6,
-        "continuousEnable": 0,
-        "continuousType": 0,
-        "expirationDate": "6666-06-06 06:06:06",
-        "superExpireDate": "6666-06-06 06:06:06",
-        "adFreeExpireDate": "6666-06-06 06:06:06",
-        "normalExpireDate": "6666-06-06 06:06:06",
-        "isPermanentVip": true,
-        "freeSendAdFreeVipEnable": 0,
-        "vipType": 2,
-        "expireDaysDays": null,
-        "freeSendVipEnable": 0
-      }
-    }
-  }
-  "error": null,
-  "needRetry": null
-}
-$done({ body: JSON.stringify(guding) });
-*/
